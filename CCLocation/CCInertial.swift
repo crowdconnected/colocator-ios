4cee8772cfc799864ddc3af898613bb8e470f3c7
Ralf Kernchen <ralf@accelogress.com>
2019-06-20 10:35:01 +0100

    linked data gathering with back-end settings and state-store

--- CCLocation/CCInertial.swift
+++ CCLocation/CCInertial.swift
@@ -28,19 +28,20 @@ class CCInertial: NSObject {
     private let activityManager = CMMotionActivityManager()
     private let pedometer = CMPedometer()
     private let motion = CMMotionManager()
-
+    
     private var pedometerStartDate: Date = Date()
     private var previousPedometerData: PedometerData?
     private var yawDataBuffer: [YawData] = []
     
     private var isInertialRunning = false
+    private var interval: Double = -1
     
     fileprivate var currentInertialState: InertialState!
     
     weak var stateStore: Store<LibraryState>!
     
     public weak var delegate:CCInertialDelegate?
-
+    
     init(stateStore: Store<LibraryState>) {
         super.init()
         
@@ -53,83 +54,104 @@ class CCInertial: NSObject {
     
     internal func startCountingSteps() {
         
-        pedometerStartDate = Date()
-        
-        previousPedometerData = PedometerData(endDate: pedometerStartDate, numberOfSteps: 0)
-        
-        pedometer.startUpdates(from: pedometerStartDate) {
-            [weak self] pedometerData, error in
-            guard let pedometerData = pedometerData, error == nil else { return }
+        if CMPedometer.isStepCountingAvailable(){
             
-            DispatchQueue.main.async {
-                let endDate = pedometerData.endDate
-                let numberOfSteps = pedometerData.numberOfSteps.intValue
-
-                if let previousPedometerData = self?.previousPedometerData {
-                    if numberOfSteps != previousPedometerData.numberOfSteps {
-                        
-                        let periodBetweenStepCounts:TimeInterval = endDate.timeIntervalSince1970 - previousPedometerData.endDate.timeIntervalSince1970
-                        
-                        let stepsBetweenStepCounts = numberOfSteps - previousPedometerData.numberOfSteps
+            pedometerStartDate = Date()
+            
+            previousPedometerData = PedometerData(endDate: pedometerStartDate, numberOfSteps: 0)
+            
+            pedometer.startUpdates(from: pedometerStartDate) {
+                [weak self] pedometerData, error in
+                guard let pedometerData = pedometerData, error == nil else {
                     
-                        let timeIntervals = TimeInterval(periodBetweenStepCounts / Double(stepsBetweenStepCounts))
-
-                        Log.debug("Step count: \(numberOfSteps), previous: \(previousPedometerData.numberOfSteps), Period between step counts: \(periodBetweenStepCounts), Steps in-between: \(stepsBetweenStepCounts), Timeintervals: \(timeIntervals), Previous timestamp: \(previousPedometerData.endDate.timeIntervalSince1970), Current timestamp: \(endDate.timeIntervalSince1970)")
-                        
-                        for i in  1 ... stepsBetweenStepCounts {
-                            let tempTimePeriod = TimeInterval(Double(i) * timeIntervals)
-                            let tempTimeStamp = previousPedometerData.endDate.timeIntervalSince1970 + tempTimePeriod
+                    Log.error("Received: \(error.debugDescription)")
+                    
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    let endDate = pedometerData.endDate
+                    let numberOfSteps = pedometerData.numberOfSteps.intValue
+                    
+                    if let previousPedometerData = self?.previousPedometerData {
+                        if numberOfSteps != previousPedometerData.numberOfSteps {
                             
-                            if let yawDataBuffer = self?.yawDataBuffer {
-                                if let tempYaw = self?.findFirstSmallerYaw(yawArray: yawDataBuffer, timeInterval: tempTimeStamp){
-                                    Log.debug ("Step count: \(i), Time period: \(tempTimePeriod), Timestamp: \(tempTimeStamp), Yaw value: \(String(describing: tempYaw.yaw))")
+                            let periodBetweenStepCounts:TimeInterval = endDate.timeIntervalSince1970 - previousPedometerData.endDate.timeIntervalSince1970
+                            
+                            let stepsBetweenStepCounts = numberOfSteps - previousPedometerData.numberOfSteps
+                            
+                            let timeIntervals = TimeInterval(periodBetweenStepCounts / Double(stepsBetweenStepCounts))
+                            
+                            Log.debug("Step count: \(numberOfSteps), previous: \(previousPedometerData.numberOfSteps), Period between step counts: \(periodBetweenStepCounts), Steps in-between: \(stepsBetweenStepCounts), Timeintervals: \(timeIntervals), Previous timestamp: \(previousPedometerData.endDate.timeIntervalSince1970), Current timestamp: \(endDate.timeIntervalSince1970)")
+                            
+                            for i in  1 ... stepsBetweenStepCounts {
+                                let tempTimePeriod = TimeInterval(Double(i) * timeIntervals)
+                                let tempTimeStamp = previousPedometerData.endDate.timeIntervalSince1970 + tempTimePeriod
                                 
-                                    self?.delegate?.receivedStep(date: tempYaw.date, angle: tempYaw.yaw)
+                                if let yawDataBuffer = self?.yawDataBuffer {
+                                    if let tempYaw = self?.findFirstSmallerYaw(yawArray: yawDataBuffer, timeInterval: tempTimeStamp){
+                                        Log.debug ("Step count: \(i), Time period: \(tempTimePeriod), Timestamp: \(tempTimeStamp), Yaw value: \(String(describing: tempYaw.yaw))")
+                                        
+                                        let stepDate = Date(timeIntervalSince1970: tempTimeStamp)
+                                        
+                                        if let angle = self?.rad2deg(tempYaw.yaw) {
+                                            self?.delegate?.receivedStep(date: stepDate, angle: angle)
+                                        }
+                                    }
                                 }
                             }
+                            
+                            self?.previousPedometerData = PedometerData (endDate: pedometerData.endDate,
+                                                                         numberOfSteps: numberOfSteps)
                         }
-                        
-                        self?.previousPedometerData = PedometerData (endDate: pedometerData.endDate,
-                                                                     numberOfSteps: numberOfSteps)
-                        
-//                        self?.yawDataBuffer.removeAll()
                     }
                 }
-//                let dataStringPart1 = "Pedometer data: " + start + end + receivedTime + numberOfSteps
             }
         }
     }
     
     private func startMotionUpdates() {
         if motion.isDeviceMotionAvailable {
-            self.motion.deviceMotionUpdateInterval = 1.0 / 5.0
+            self.motion.deviceMotionUpdateInterval = interval
             self.motion.showsDeviceMovementDisplay = true // need to double check the behaviour on this one
-            self.motion.startDeviceMotionUpdates(using: .xArbitraryZVertical, to: OperationQueue.main, withHandler: { (data, error) in
-                if let validData = data {
+            self.motion.startDeviceMotionUpdates(using: .xArbitraryZVertical, to: OperationQueue.main, withHandler: { (deviceMotion, error) in
+                
+                guard let data = deviceMotion, error == nil else {
                     
-                    let yawValue = validData.attitude.yaw
-                    let yawData = YawData(yaw: yawValue, date: Date())
+                    let cmError = error as? CMError
                     
-                    Log.debug("Yaw:\(yawData.yaw), Timestamp: \(yawData.date.timeIntervalSince1970)")
+                    Log.error("Received: \(cmError.debugDescription)")
                     
-                    self.yawDataBuffer.append(yawData)
+                    return
                 }
+                
+                
+                let yawValue = data.attitude.yaw
+                let yawData = YawData(yaw: yawValue, date: Date())
+                
+//                    Log.debug("Yaw:\(yawData.yaw), Timestamp: \(yawData.date.timeIntervalSince1970), Interval: \(self.interval)")
+                
+                self.yawDataBuffer.append(yawData)
             })
         }
     }
     
     private func findFirstSmallerYaw (yawArray:[YawData], timeInterval: TimeInterval) -> YawData?{
         for (index, yaw) in yawArray.reversed().enumerated() {
-//            Log.debug("Yaw time interval: \(yaw.date.timeIntervalSince1970), Time interval: \(timeInterval)")
+            //            Log.debug("Yaw time interval: \(yaw.date.timeIntervalSince1970), Time interval: \(timeInterval)")
             if yaw.date.timeIntervalSince1970 < timeInterval {
-                yawDataBuffer.removeSubrange(0 ... index)
+//                yawDataBuffer.removeSubrange(index ... yawDataBuffer.count - 1)
                 Log.debug("Yaw index: \(index)")
                 return yaw
             }
         }
         return nil
     }
-
+    
+    private func rad2deg(_ number: Double) -> Double {
+        return number * 180 / .pi
+    }
+    
     internal func start () {
         Log.debug("Starting intertial")
         
@@ -151,23 +173,22 @@ class CCInertial: NSObject {
 extension CCInertial: StoreSubscriber {
     public func newState(state: LibraryState) {
         if let newInertialState = state.intertialState {
-
+            
             Log.debug("new state is: \(newInertialState)")
             
             if newInertialState != self.currentInertialState {
                 self.currentInertialState = newInertialState
                 Log.debug("new state is: \(newInertialState)")
                 
+                if let interval = newInertialState.interval {
+                    self.interval = Double(interval) / 1000
+                }
+
                 if let isInertialEnabled = newInertialState.isEnabled {
                     if isInertialEnabled {
                         start()
                     } else {
-
-// just for testing !!!!!!!!!!!!
-
-                        start()
-                        
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                        stop()
                     }
                 }
             }
