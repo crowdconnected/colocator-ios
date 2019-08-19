45dc54167db1b3e5e3338865e2b4c63386790ef8
Robert Tanase <robert@crowdconnected.com>
2019-08-19 11:19:38 +0100

    Added feature for sending data when stopping library

--- CCLocation/ColocatorManager.swift
+++ CCLocation/ColocatorManager.swift
@@ -25,6 +25,9 @@ class ColocatorManager {
     var ccInertial: CCInertial?
     var ccSocket: CCSocket?
     
+    var stopLibraryTimer: Timer?
+    var secondsFromStopTrigger = 0
+    
     public static let sharedInstance: ColocatorManager = {
         let instance = ColocatorManager()
         return instance
@@ -37,6 +40,13 @@ class ColocatorManager {
         if !running {
             
             running = true
+            
+            if stopLibraryTimer != nil {
+                destroyConnection()
+            }
+            stopLibraryTimer?.invalidate()
+            stopLibraryTimer = nil
+            
             startTime = Date()
             deviceId = UserDefaults.standard.string(forKey: CCSocketConstants.LAST_DEVICE_ID_KEY)
            
@@ -55,6 +65,8 @@ class ColocatorManager {
             ccLocationManager!.delegate = self
             ccInertial!.delegate = self
             
+            Log.info("[Colocator] Attempt to connect to back-end with URL: \(urlString) and APIKey: \(apiKey)")
+                       
             ccSocket?.start(urlString: urlString,
                             apiKey: apiKey,
                             ccRequestMessaging: ccRequestMessaging!)
@@ -81,14 +93,57 @@ class ColocatorManager {
             ccLocationManager?.stop()
             ccLocationManager?.delegate = nil
             ccLocationManager = nil
-            ccRequestMessaging = nil
             
-            ccSocket?.stop()
-            ccSocket?.delegate = nil
-            ccSocket = nil
-            
-            Log.debug("[Colocator] Stopping Colocator")
+            Log.info("Sending all messages from local database to server before stopping")
+        
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.ccRequestMessaging?.sendQueuedMessagesAndStopTimer()
+            }
+        
+            stopLibraryTimer = Timer.scheduledTimer(timeInterval: 1.0,
+                                                    target: self,
+                                                    selector: #selector(checkDatabaseAndStopLibrary),
+                                                    userInfo: nil,
+                                                    repeats: true)
         }
+    }
+    
+    @objc private func checkDatabaseAndStopLibrary() {
+        secondsFromStopTrigger += 1
+        
+        if secondsFromStopTrigger > 60 {
+            let leftMessages = ccRequestMessaging?.getMessageCount() ?? 0
+            Log.warning("Library stopped. \(leftMessages) unsent messages")
+            
+            destroyConnection()
+            secondsFromStopTrigger = 0
+            return
+        }
+        
+        if checkDatabaseEmptiness() {
+            destroyConnection()
+        }
+    }
+    
+    private func checkDatabaseEmptiness() -> Bool {
+        if let messagesLeft = ccRequestMessaging?.getMessageCount() {
+            return messagesLeft == 0
+        }
+        return true
+    }
+    
+    private func destroyConnection() {
+        stopLibraryTimer?.invalidate()
+        stopLibraryTimer = nil
+        
+        ccRequestMessaging?.stop()
+        ccRequestMessaging = nil
+        
+        ccSocket?.stop()
+        ccSocket?.delegate = nil
+        ccSocket = nil
+        
+        Log.info("[Colocator] Active back-end connection destroyed")
     }
     
     public func stopLocationObservations() {
