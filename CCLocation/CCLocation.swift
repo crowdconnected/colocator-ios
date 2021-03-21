c857e4b4a3b8b063a30450f7d020c427e42bb6ba
Robert Tanase <robert.tanase97@gmail.com>
2021-03-21 10:50:15 +0000

    Update logger levels when the library starts

--- CCLocation/CCLocation.swift
+++ CCLocation/CCLocation.swift
@@ -53,7 +53,7 @@ internal struct Constants {
         if libraryStarted == false {
             libraryStarted = true
             
-            setLoggerLevels(verbose: false, info: true, debug: true, warning: true, error: true, severe: true)
+            setLoggerLevels(verbose: false, info: false, debug: false, warning: true, error: true, severe: true)
             
             NSLog("[Colocator] Initialising Colocator")
             
